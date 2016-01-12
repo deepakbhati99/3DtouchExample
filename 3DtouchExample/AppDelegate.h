3d50c8c6c120099a7308735127a3f45c80ebc874
deepakbhati99 <Khatri@khatris-mac-mini.local>
2016-01-12 16:58:03 +0530

    Initial Commit

---
+++ 3DtouchExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DtouchExample
+//
+//  Created by Himanshu Khatri on 1/12/16.
+//  Copyright © 2016 bd 001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3DtouchExample/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  3DtouchExample
+//
+//  Created by Himanshu Khatri on 1/12/16.
+//  Copyright © 2016 bd 001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
